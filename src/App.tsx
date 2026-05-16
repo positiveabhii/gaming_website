8463d5cc6c76b8399995f8c11c4f691d0006eda9
Abhi Jain <137091099+positiveabhii@users.noreply.github.com>
2026-05-16 08:56:25 +0530

    Remove contact hover functionality and routes

--- src/App.tsx
+++ src/App.tsx
@@ -19,72 +19,7 @@ function App() {
 
   return (
     <div className="App relative">
-      <Navbar />
-      <div
-        className="fixed z-[1000] flex items-center gap-3 rounded-full cursor-pointer"
-        style={{
-          bottom: "200px",
-          right: "2px",
-          backgroundColor: hoveringOnContact ? "#4a4a4a" : "#ffa115",
-          padding: "10px",
-        }}
-        onMouseEnter={() => sethoveringOnContact(true)}
-        onMouseLeave={() => sethoveringOnContact(false)}
-      >
-        {hoveringOnContact && (
-          <>
-            <a
-              href="http://wa.me/919830601360"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FontAwesomeIcon
-                icon={faWhatsapp}
-                color={hoveringOnContact ? "#ffa115" : "#232323"}
-                size="2x"
-              />
-            </a>
-            <a
-              href="https://www.linkedin.com/company/p-p-v/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FontAwesomeIcon
-                icon={faLinkedin}
-                color={hoveringOnContact ? "#ffa115" : "#232323"}
-                size="2x"
-              />
-            </a>
-          </>
-        )}
-        {hoveringOnContact ? (
-          <a href="mailto:contact@platformplay.in">
-            <FontAwesomeIcon
-              icon={faEnvelope}
-              color={hoveringOnContact ? "#ffa115" : "#232323"}
-              size="2x"
-            />
-          </a>
-        ) : (
-          <FontAwesomeIcon
-            icon={faEnvelope}
-            color={hoveringOnContact ? "#ffa115" : "#232323"}
-            size="2x"
-          />
-        )}
-      </div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/founder" element={<Founder />} />
-        <Route path="/solution/:category" element={<Gaming />} />
-        <Route path="/our-partners" element={<Partner />} />
-        <Route path="/contactus" element={<Contact />} />
-        <Route path="/insights" element={<Insights />} />
-        <Route element={<CS />} path="/insights/case-studies/:id" />
-        <Route path="/insights/blog/:id" element={<BlogComponent />} />
-        {/* Catch-all route for handling incorrect routes */}
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+       website in accessible 
     </div>
   );
 }
